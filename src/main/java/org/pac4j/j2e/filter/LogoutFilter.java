c422fa31d3f79254b5e580bf182c56cc9669b9cc
leleuj <leleuj@gmail.com>
2017-05-21 14:28:07 +0200

    Add constructors to filters

--- src/main/java/org/pac4j/j2e/filter/LogoutFilter.java
+++ src/main/java/org/pac4j/j2e/filter/LogoutFilter.java
@@ -19,11 +19,15 @@ import static org.pac4j.core.util.CommonHelper.*;
 /**
  * <p>This filter handles the (application + identity provider) logout process, based on the {@link #logoutLogic}.</p>
  *
- * <p>The configuration can be provided via servlet parameters: <code>defaultUrl</code> (default logourl url),<code>logoutUrlPattern</code> (pattern that logout urls must match),
- * <code>localLogout</code> (whether the application logout must be performed), <code>destroySession</code> (whether we must destroy the web session during the local logout)
- * and <code>centralLogout</code> (whether the centralLogout must be performed).</p>
- * <p>Or it can be defined via setter methods: {@link #setDefaultUrl(String)}, {@link #setLogoutUrlPattern(String)}, {@link #setLocalLogout(Boolean)},
- * {@link #setDestroySession(Boolean)} and {@link #setCentralLogout(Boolean)}.</p>
+ * <p>The configuration can be provided via servlet parameters, setters or constructors for the following options:</p>
+ * <ul>
+ *     <li><code>configFactory</code> (the class name of the factory to build the configuration) or <code>config</code> (the configuration itself)</li>
+ *     <li><code>defaultUrl</code> (default logourl url)</li>
+ *     <li><code>logoutUrlPattern</code> (pattern that logout urls must match)</li>
+ *     <li><code>localLogout</code> (whether the application logout must be performed)</li>
+ *     <li><code>destroySession</code> (whether we must destroy the web session during the local logout)</li>
+ *     <li><code>centralLogout</code> (whether the centralLogout must be performed)</li>
+ * </ul>
  *
  * @author Jerome Leleu
  * @since 1.2.0
@@ -42,8 +46,19 @@ public class LogoutFilter extends AbstractConfigFilter {
 
     private Boolean centralLogout;
 
+    public LogoutFilter(final Config config) {
+        setConfig(config);
+    }
+
+    public LogoutFilter(final Config config, final String defaultUrl) {
+        this(config);
+        this.defaultUrl = defaultUrl;
+    }
+
     @Override
     public void init(final FilterConfig filterConfig) throws ServletException {
+        super.init(filterConfig);
+
         this.defaultUrl = getStringParam(filterConfig, Pac4jConstants.DEFAULT_URL, this.defaultUrl);
         this.logoutUrlPattern = getStringParam(filterConfig, Pac4jConstants.LOGOUT_URL_PATTERN, this.logoutUrlPattern);
         this.localLogout = getBooleanParam(filterConfig, Pac4jConstants.LOCAL_LOGOUT, this.localLogout);
